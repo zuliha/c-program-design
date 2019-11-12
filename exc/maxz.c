f09550197150e708cb93dc25481f5fec5f558003
daxia <419163530@qq.com>
2019-11-12 21:38:34 +0800

    exercises

---
+++ exc/maxz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int max(int x, int y);
+    int a, b, c;
+    printf("请输入两个整数:\n");
+    scanf("%d %d", &a, &b);
+    c = max(a, b);
+    printf("max=%d\n", c);
+    return 0;
+}
+int max(int x, int y)
+{
+    int z;
+    if (x > y)
+        z = x;
+    else
+        z = y;
+    return z;
+} /* exc git:(master) ✗ clang maxz.c
+➜  exc git:(master) ✗ ./a.out     
+请输入两个整数:
+8 963
+max=963*/
