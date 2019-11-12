06ceae1edfde5bccdc7b8a2b6dd1e0efe4b86d0e
daxia <419163530@qq.com>
2019-11-12 15:21:54 +0800

    练习题

---
+++ exc/abc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swap(a,b,c)
+int main()
+{
+    int a,b,c;
+
+    scanf("%d%d%d\n",&a,&b,&c);
+    if (a>c)
+    {
+        swap(a,c);
+    }
+    if(b>c)
+    {
+        swap(b,c);
+    }
+    if(a>b)
+    {
+        swap(a,b);
+    }
+    printf("按从大到小顺序为:%d%d%d\n",a,b,c);
+    return 0;
+}
