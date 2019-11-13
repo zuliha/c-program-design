611701d231e1aa0d2b0dbda1ca6a3f132a3b59e4
daxia <419163530@qq.com>
2019-11-13 13:06:18 +0800

    first version submision

--- ch5/ch5.1.c
+++ ch5/ch5.1.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-int main(){
-    int i=1,sum=0;
-    
-    while(i<101)
+#include <stdio.h>
+int main()
+{
+    int i = 1, sum = 0;
+
+    while (i < 101)
     {
-        sum=sum+i;
+        sum = sum + i;
         i++;
     }
-    printf("和为:%d\n",sum);
+    printf("%d\n", sum);
     return 0;
-
 }
+/*➜  ch5 git:(master) ✗ clang ch5.1.c 
+➜  ch5 git:(master) ✗ ./a.out 
+5050*/
