bd498740d01371de53937b403621c01710a018f5
daxia <419163530@qq.com>
2019-11-13 13:06:00 +0800

    first version submision

--- ch5/ch5.6.c
+++ ch5/ch5.6.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,j,n=0;
-    for(i=1;i<5;i++)
-        for(j=1;j<6;j++,n++){
-        if(n%5==0)printf("\n");
-        if(i==3 && j==1)break;
-        printf("%d\t",i*j);
+    int i, j, n = 0;
+    for (i = 1; i < 5; i++)
+        for (j = 1; j < 6; j++, n++)
+        {
+            if (n % 5 == 0)
+                printf("\n");
+            if (i == 3 && j == 1)
+                break;
+            printf("%d\t", i * j);
+        }
+    printf("\n");
+    return 0;
+}
+/*➜  ch5 git:(master) ✗ clang ch5.6.c
+➜  ch5 git:(master) ✗ ./a.out      
 
-}
-printf("\n");
-return 0;
-}
+1       2       3       4       5
+2       4       6       8       10
+
+4       8       12      16      20*/
