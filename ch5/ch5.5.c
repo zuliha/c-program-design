94d4dd2fa285ee5f056e1d4aa80dc4a599ee487f
daxia <419163530@qq.com>
2019-11-13 11:51:16 +0800

    first version submission

---
+++ ch5/ch5.5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, n = 0;
+    for (i = 1; i < 5; i++)
+        for (j = 1; j < 6; j++, n++)
+        {
+            if (n % 5 == 0)
+                printf("\n");
+
+            printf("%d\t", i * j);
+        }
+    printf("\n");
+    return 0;
+}/*➜  ch5 git:(master) ✗ clang ch5.5.c
+➜  ch5 git:(master) ✗ ./a.out      
+
+1       2       3       4       5
+2       4       6       8       10
+3       6       9       12      15
+4       8       12      16      20
+*/
