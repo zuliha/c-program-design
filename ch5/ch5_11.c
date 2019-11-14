e53a5497426eebf1f0464e120b76f733f9603364
daxia <419163530@qq.com>
2019-11-14 17:04:17 +0800

    求三个公式的和

---
+++ ch5/ch5_11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    double s1 = 0, s2 = 0, s3 = 0, b;
+    int  i, a;
+    for (i = 1; i < 101; i++)
+    {
+        s1 += i;
+    }
+    for (a = 1; a < 51; a++)
+    {
+
+        s2 += a * a;
+    }
+    for (b = 1; b < 11; b++)
+    {
+        s3 += 1 / b;
+    }
+    printf("%lf %lf %lf\n", s1, s2, s3);
+    printf("%15.6f\n", s1 + s2 + s3);
+    return 0;
+}
