a1538a4cf0f18a9133e3fa087a3454719ecf09b9
daxia <419163530@qq.com>
2019-11-15 21:06:50 +0800

    调整结果格式

--- ch5/ch5.7.c
+++ ch5/ch5.7.c
@@ -14,11 +14,11 @@ int main()
     printf("\n");
     return 0;
 }
-/*➜  ch5 git:(master) ✗ clang ch5.7.c
-➜  ch5 git:(master) ✗ ./a.out 
+/*
 
 0       0       0       0       0
 1       2       3       4       5
 2       4       6       8       10
 6       9       12      15
-4       8       12      16      20*/
+4       8       12      16      20
+*/
