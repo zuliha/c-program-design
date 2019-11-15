547ad27b83ad978741a754f0fc04609903057a05
daxia <419163530@qq.com>
2019-11-15 13:55:18 +0800

    添加第六章练习题

---
+++ ch6/c6_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i, a[10];
+    for (i = 0; i <= 9; i++)
+    {
+        a[i] = i;
+    }
+    for (i = 9; i >= 0; i--)
+    {
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
+// 9876543210 
