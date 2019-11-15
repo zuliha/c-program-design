746aa67dbb8e8439d4014d09b621f7e31c4cd739
daxia <419163530@qq.com>
2019-11-15 20:44:53 +0800

    调整结果格式

--- ch3/float.c
+++ ch3/float.c
@@ -1,18 +1,20 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    float x,y;
-    char c1,c2;
-    scanf("%d%d",&a,&b);
-    printf("a=%d,b=%d\n",a,b);
-    scanf("%f%f",&x,&y);
-    printf("x=%f,y=%f\n",x,y);
-    scanf("%c%c",&c1,&c2);
-    printf("c1=%c,c2=%c\n",c1,c2);
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    float x, y;
+    char c1, c2;
+    scanf("%d%d", &a, &b);
+    printf("a=%d,b=%d\n", a, b);
+    scanf("%f%f", &x, &y);
+    printf("x=%f,y=%f\n", x, y);
+    scanf("%c%c", &c1, &c2);
+    printf("c1=%c,c2=%c\n", c1, c2);
     return 0;
-} 
-/*ch3 git:(master) ✗ ./a.out
+}
+/*
 3 7 8.5 71.82Aa
 a=3,b=7
 x=8.500000,y=71.820000
-c1=A,c2=a*/
+c1=A,c2=a
+*/
