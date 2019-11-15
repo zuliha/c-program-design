4df7a7bdbcf2ac57b072edf4e64bdd2183989350
daxia <419163530@qq.com>
2019-11-15 20:27:31 +0800

    调整代码格式

--- ch2/exchange.c
+++ ch2/exchange.c
@@ -1,17 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b,c;
-    a=0;
-    b=1;
-    c=a;
-    a=b;
-    b=c;
-    printf("%d %d\n",a,b);
-    return 0;
-   /*  gcc ch2/exchange.c
- c git:(master) ✗ ./a.out 
-1 0
- */
-
+  int a, b, c;
+  a = 0;
+  b = 1;
+  c = a;
+  a = b;
+  b = c;
+  printf("%d %d\n", a, b);
+  return 0;
 }
+//1 0
