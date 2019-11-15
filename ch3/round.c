d5104bd01937b4092ebf352986121efdbde7871d
daxia <419163530@qq.com>
2019-11-15 20:50:19 +0800

    调整结果格式

--- ch3/round.c
+++ ch3/round.c
@@ -1,27 +1,28 @@
-#include<stdio.h>
-int main(){
-    float p=3.14,r,s1,s2,v1,v2,c;
+#include <stdio.h>
+int main()
+{
+    float p = 3.14, r, s1, s2, v1, v2, c;
     int h;
     printf("输入圆的半径r,高h\n");
-    scanf("%f %d",&r,&h);
-    s1=p*r*r;
-    s2=4*p*r*r;
-    v1=4/3*p*r*r*r;
-    v2=p*r*r*h;
-    c=2*p*r;
-    printf("圆的面积为%6.2f\n",s1);
-    printf("圆球的面积为%6.2f\n",s2);
-    printf("圆球的体积为%6.2f\n",v1);
-    printf("圆柱的体积为%6.2f\n",v2);
-    printf("圆的周长为%6.2f\n",c);
+    scanf("%f %d", &r, &h);
+    s1 = p * r * r;
+    s2 = 4 * p * r * r;
+    v1 = 4 / 3 * p * r * r * r;
+    v2 = p * r * r * h;
+    c = 2 * p * r;
+    printf("圆的面积为%6.2f\n", s1);
+    printf("圆球的面积为%6.2f\n", s2);
+    printf("圆球的体积为%6.2f\n", v1);
+    printf("圆柱的体积为%6.2f\n", v2);
+    printf("圆的周长为%6.2f\n", c);
     return 0;
-
-}/*➜  ch3 git:(master) ✗ clang round.c
-➜  ch3 git:(master) ✗ ./a.out      
+}
+/*
 输入圆的半径r,高h
 1.5 3
 圆的面积为  7.07
 圆球的面积为 28.26
 圆球的体积为 10.60
 圆柱的体积为 21.19
-圆的周长为  9.42*/
+圆的周长为  9.42
+*/
