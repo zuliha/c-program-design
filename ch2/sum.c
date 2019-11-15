95625e6d7f48d955aa068cb593862fa6f928f27b
daxia <419163530@qq.com>
2019-11-15 20:34:22 +0800

    调整代码格式

--- ch2/sum.c
+++ ch2/sum.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-int main(){
-    int sum=0;
+#include <stdio.h>
+int main()
+{
+    int sum = 0;
     int i;
-    for(i=1;i<=100;i++)
-    sum=sum+i;
-    printf("1~100的和sum=%d\n",sum);
+    for (i = 1; i <= 100; i++)
+        sum = sum + i;
+    printf("1~100的和sum=%d\n", sum);
     return 0;
 }
-/*➜  ch2 git:(master) ✗ gcc sum.c
-➜  ch2 git:(master) ✗ ./a.out 
-1~100的和sum=5050*/
+//1~100的和sum=5050
