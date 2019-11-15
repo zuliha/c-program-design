de8c6fafb7b3e0caf2f53c58fb94a849d4c88370
daxia <419163530@qq.com>
2019-11-15 20:31:44 +0800

    调整代码格式

--- ch2/max.c
+++ ch2/max.c
@@ -1,23 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b,max;
-    scanf("%d %d",&a,&b);
-    if(a>b)
+    int a, b, max;
+    scanf("%d %d", &a, &b);
+    if (a > b)
     {
         printf("max=a\n");
-    }  
+    }
     else
         printf("max=b\n");
-        return 0;
+    return 0;
 }
-
-/* clang max.c 
-➜  ch2 git:(master) ✗ ./a.out 
+/*
 2 3
 max=b
-➜  ch2 git:(master) ✗ 6 8
-cd: string not in pwd: -6
-➜  ch2 git:(master) ✗ ./a.out
 9 1
-max=a*/
+max=a
+*/
