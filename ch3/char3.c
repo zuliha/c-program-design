a8de849f49bf19d50235ad827a51b31b151390f6
daxia <419163530@qq.com>
2019-11-15 20:41:49 +0800

    调整结果格式

--- ch3/char3.c
+++ ch3/char3.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int c1,c2;
-    c1=97;
-    c2=98;
-    printf("c1=%c,c2=%c\n",c1,c2);
-    printf("c1=%d,c2=%d\n",c1,c2);
+    int c1, c2;
+    c1 = 97;
+    c2 = 98;
+    printf("c1=%c,c2=%c\n", c1, c2);
+    printf("c1=%d,c2=%d\n", c1, c2);
     return 0;
 }
-/*c1=-59,c2=-58
-➜  ch3 git:(master) ✗ clang char3.c
-➜  ch3 git:(master) ✗ ./a.out      
+/*
+c1=-59,c2=-58
 c1=a,c2=b
-c1=97,c2=98*/
+c1=97,c2=98
+*/
