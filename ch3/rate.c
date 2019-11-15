2fcd4994f2a16c7b8e5ea680b201c2e4561ee3ad
daxia <419163530@qq.com>
2019-11-15 20:49:33 +0800

    调整结果格式

--- ch3/rate.c
+++ ch3/rate.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-   // printf("十年后是现在的%f倍",pow(1.09,10));
-    float r=0.09;
-    int n=10;
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // printf("十年后是现在的%f倍",pow(1.09,10));
+    float r = 0.09;
+    int n = 10;
     float p;
-    p=powf(1+r,n);
-    printf("10年后是现在的%f倍\n",p);
-    printf("%d,%f\n",n,r);
+    p = powf(1 + r, n);
+    printf("10年后是现在的%f倍\n", p);
+    printf("%d,%f\n", n, r);
     return 0;
 }
-/*➜  ch3 git:(master) ✗ clang rate.c -lm
-➜  ch3 git:(master) ✗ ./a.out 
+/*
 10年后是现在的2.367364倍
-10,0.090000*/
+10,0.090000
+*/
