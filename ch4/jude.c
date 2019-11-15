5454c5e17069241ad2480ac2b431dc56ce573862
daxia <419163530@qq.com>
2019-11-15 20:55:32 +0800

    调整结果格式

--- ch4/jude.c
+++ ch4/jude.c
@@ -1,26 +1,24 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int a,p;
-    scanf("%d",&a);
-    if((a>1000)&&(a<0))
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, p;
+    scanf("%d", &a);
+    if ((a > 1000) && (a < 0))
     {
         printf("请重新输入\n");
-    } 
+    }
     else
     {
-    p=sqrt(a);
+        p = sqrt(a);
     }
-    printf("%d\n",p);
+    printf("%d\n", p);
     return 0;
-
 }
 /*
-➜  ch4 git:(master) ✗ clang jude.c -lm
-➜  ch4 git:(master) ✗ ./a.out 
 9
 3
-➜  ch4 git:(master) ✗ ./a.out
+
 625
 25
 */
