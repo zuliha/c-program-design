ff8eb734902df4cba723352287bfb2a9ddfe1bed
daxia <419163530@qq.com>
2019-11-16 10:53:49 +0800

    添加第五章练习题

---
+++ ch5/ch5_13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float x0, x1, a;
+    scanf("%f", &a);
+    x0 = a / 2;
+    x1 = (x0 + a / x0) / 2;
+    do
+    {
+        x0 = x1;
+        x1 = (x0 + a / x0) / 2;
+    } while (fabs(x0 - x1) >= 1e-5);
+    printf("The square root of %f is %f\n", a, x1);
+    return 0;
+}
+/* 
+2
+The square root of 2.000000 is 1.414214 
+*/
