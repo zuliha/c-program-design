0ec93bafbfff408ccc6a71cc79893db3ccee3fb9
daxia <419163530@qq.com>
2019-11-16 10:05:34 +0800

    添加第七章练习题

---
+++ ch7/c7_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int age(int n);
+    printf("No.5 age is %d\n",age(5));
+    return 0;
+}
+int age(int n)
+{
+    int c;
+    if (n == 1)
+        c = 10;
+    else
+        c = age(n - 1) + 2;  //n>1
+    return c;
+}
+//No.5 age is 18
