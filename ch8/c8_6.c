679603dc6597bca26aa3bc68e2129055db89df30
daxia <419163530@qq.com>
2019-11-18 20:55:43 +0800

    添加第八章练习题

---
+++ ch8/c8_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int *p, a[10];
+    printf("enter ten numbers:");
+
+    for (int i = 0; i < 10; i++)
+        scanf("%d", a + i);
+    for (p = a; p < a + 10; p++)
+        printf("%d", *p);
+    printf("\n");
+    return 0;
+}
+/* 
+enter ten numbers:0 1 2 3 4 5 6 7 8 9
+0123456789 
+*/
