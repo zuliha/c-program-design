c12cba7847844fb2c023d4ffdc69a0e3eae3ad8e
daxia <419163530@qq.com>
2019-11-18 16:19:50 +0800

    添加第八章练习题

---
+++ ch8/c8_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 100, b = 10;
+    int *pointer_1, *pointer_2;
+    pointer_1 = &a;
+    pointer_2 = &b;
+    printf("%d %d",*pointer_1,*pointer_2);
+    return 0;
+}
