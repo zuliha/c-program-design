cba3fa06cce1567aaba06ce496cbdd41e1f52269
zuliha <419163530@qq.com>
2020-11-27 16:06:47 +0800

    push

--- ch8/max2.c
+++ ch8/max2.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int a, b, *p1, *p2, p;
+    int a, b, *p1, *p2, *p;
     printf("enter two numbers:\n");
     scanf("%d %d", &a, &b);
     p1 = &a;
     p2 = &b;
     if (a < b)
     {
-        p = *p1;
-        *p1 = *p2;
-        *p2 = p;
+        p = p1;
+        p1 = p2;
+        p2 = p;
     }
     printf("max=%d\nmin=%d\n", *p1, *p2);
     return 0;
