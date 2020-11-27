da6e809a393ebd383414abf1e406417744293269
zuliha <419163530@qq.com>
2020-11-27 16:02:24 +0800

    push

---
+++ ch8/max2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, *p1, *p2, p;
+    printf("enter two numbers:\n");
+    scanf("%d %d", &a, &b);
+    p1 = &a;
+    p2 = &b;
+    if (a < b)
+    {
+        p = *p1;
+        *p1 = *p2;
+        *p2 = p;
+    }
+    printf("max=%d\nmin=%d\n", *p1, *p2);
+    return 0;
+}
+/*
+enter two numbers:
+34 67
+max=67
+min=34
+*/
