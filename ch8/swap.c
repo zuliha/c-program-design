167afea02bdab1dccb080f3a77ffd65b88ec76a3
zuliha <419163530@qq.com>
2020-11-28 20:13:52 +0800

    push

--- ch8/swap.c
+++ ch8/swap.c
@@ -22,11 +22,16 @@ int main()
         swap(p1, p2);
     }
     printf("max=%d\nmin=%d\n", *p1, *p2);
+    printf("-----------------------\n");
+    printf("max=%d\nmin=%d\n", a, b);           //a,b的值改变，而p1和p2的值不变
     return 0;
 }
 /*
 enter two numbers:
 34 87
+max=87
+min=34
+-----------------------
 max=87
 min=34
 */
