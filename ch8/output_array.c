1cd37272161465fe76d90bb7c777907c9a770ab1
zuliha <419163530@qq.com>
2020-11-28 19:06:22 +0800

    Submit output array code

---
+++ ch8/output_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a[10];
+    printf("enter ten numbers:");
+
+    for (int i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+    for (int i = 0; i < 10; i++)
+        printf("%d  ", a[i]);
+    printf("\n");
+    return 0;
+}
+/* enter ten numbers:0 1 2 3 4 5 6 7 8 9
+0  1  2  3  4  5  6  7  8  9  
+*/
