f01c6da346aefd639325fe0da00f6cf495c2bdcb
zuliha <419163530@qq.com>
2020-11-30 10:46:27 +0800

    Pointer variable with n elements

---
+++ ch8/array2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int (*p)[4], i, j;           //p为指向包含4个整型元素的指针变量
+    p = arr;
+    printf("enter row and colum:");
+    scanf("%d %d", &i, &j);
+    printf("a[%d][%d]=%d\n",i,j,*(*(p+i)+j));
+    return 0;
+}
