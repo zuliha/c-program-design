11d99c1c3edb0da583a8e39a60a004b6c6b112af
zuliha <419163530@qq.com>
2021-01-10 16:51:12 +0800

    Submit structure code

--- ch9/c9_1.c
+++ ch9/c9_1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    struct student
+    struct Student                       //声明结构体类型
     {
         long int num;
         int age;
-        char name[4];
+        char name[10];
         char sex;
         char addr[20];
 
-    } a = {5, 20, "Limin", 'M', "123 Beijing Rode"};
+    } a = {5, 20, "Limin", 'M', "123 Beijing Rode"};      //定义结构体变量a并初始化
     printf("No.%ld\nage:%d\nname:%s\nsex:%c\naddr:%s\n", a.num, a.age, a.name, a.sex, a.addr);
     return 0;
 }
