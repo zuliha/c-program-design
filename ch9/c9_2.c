3c8b2f3abc54262b6e54ba12577b225a2cfe8bb5
zuliha <419163530@qq.com>
2021-01-10 19:51:27 +0800

    Submit structure code

--- ch9/c9_2.c
+++ ch9/c9_2.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
+
 int main()
 {
-    struct student
-
+    struct Student
     {
         int num;
         char name[20];
         float score;
     } student1, student2;
     scanf("%d %s %f", &student1.num, student1.name, &student1.score);
-    scanf("%d %s %f", &student2.num, student2.name, &student2.score);
+    scanf("%d %s %f", &student2.num, student2.name, &student2.score); //name是数组名，本身就代表地址，再加&就是画蛇添足
     printf("The higher score is:");
     if (student1.score > student2.score)
-        printf("%d %s %6.2f", student1.num, student1.name, student1.score);
+    {
+        printf("%d %s %5.2f\n", student1.num, student1.name, student1.score);
+    }
     else if (student1.score < student2.score)
-        printf("%d %s %6.2f", student2.num, student2.name, student2.score);
+    {
+        printf("%d %s %5.2f\n", student2.num, student2.name, student2.score);
+    }
     else
     {
-        printf("%d %s %6.2f\n", student1.num, student1.name, student1.score);
-        printf("%d %s %6.2f\n", student2.num, student2.name, student2.score);
+        printf("%d %s %5.2f\n", student1.num, student1.name, student1.score);
+        printf("%d %s %5.2f\n", student2.num, student2.name, student2.score);
     }
     return 0;
 }
-/* 
-5 han 60 
-19 wu 59
-The higher score is:5 han  60.00 
-
-5 han 60
-19 wu 61
-The higher score is:19 wu  61.00
-
-5 han 60
-19 wu 60
-The higher score is:5 han  60.00
-19 wu  60.00
+/*
+1 lilei 118
+2 hanmeimei 116
+The higher score is:1 lilei 118.00
 */
